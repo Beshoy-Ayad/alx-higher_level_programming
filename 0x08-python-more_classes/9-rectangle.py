f8e2d804d325a7d3816ed69242e45b8f70746c67
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-07-03 14:17:37 -0700

    9.2

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
-""" Definition of a class to represent a rectangle
-"""
+"""Defines a Rectangle class."""
 
 
-class Rectangle():
-    """ Represent a rectangle
+class Rectangle:
+    """Represent a rectangle.
+
     """
+
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """ Instantiate a rectangle
-        """
         type(self).number_of_instances += 1
-        self.height = height
         self.width = width
-
-    def __del__(self):
-        type(self).number_of_instances -= 1
-        print("Bye rectangle...")
-
-    def __str__(self):
-        if self.height and self.width:
-            return '\n'.join(
-                [str(self.print_symbol) * self.width] * self.height
-            )
-        return ''
-
-    def __repr__(self):
-        return 'Rectangle({}, {})'.format(self.width, self.height)
+        self.height = height
 
     @property
     def width(self):
@@ -55,12 +40,12 @@ class Rectangle():
         self.__height = value
 
     def area(self):
-        return self.width * self.height
+        return (self.__width * self.__height)
 
     def perimeter(self):
-        if self.width and self.height:
-            return 2 * (self.width + self.height)
-        return 0
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
@@ -69,9 +54,29 @@ class Rectangle():
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
+            return (rect_1)
+        return (rect_2)
 
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return (cls(size, size))
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
+
+    def __repr__(self):
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
