ffb815f6da3b319086edcbe6691d80425a05af51
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-07-11 14:24:04 -0700

    1.1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module to write a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file and returns the number of characters written"""
+    # Use the with statement to open the file in write mode
+    with open(filename, "w", encoding="utf-8") as f:
+        # Write the text to the file and return the number of characters written
+        return f.write(text)
