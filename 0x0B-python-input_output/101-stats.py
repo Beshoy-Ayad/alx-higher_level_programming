10c318ab03eba87b6f20f5806c0bfc080a411259
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-07-11 15:40:27 -0700

    101.2

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,55 +1,68 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
+"""script to read stdin line by line and compute metrics"""
 
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-"""
+import sys  # Import the sys module to access stdin
+import signal  # Import the signal module to handle keyboard interruption
+
+# Initialize a dictionary to store the status codes and their counts
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+# Initialize a variable to store the total file size
+total_size = 0
+# Initialize a variable to store the number of lines read
+line_count = 0
+
+# Define a function to print the statistics
 
 
-def print_stats(size, status_codes):
-    """Print accumulated metrics.
+def print_stats():
+    """Prints the statistics"""
 
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-    """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+    # Print the total file size
+    print("File size: {}".format(total_size))
+    # Loop through the status codes in ascending order
+    for code in sorted(status_codes.keys()):
+        # If the count is not zero, print the code and the count
+        if status_codes[code] != 0:
+            print("{}: {}".format(code, status_codes[code]))
 
-if __name__ == "__main__":
-    import sys
+# Define a function to handle keyboard interruption
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
 
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
+def signal_handler(sig, frame):
+    """Handles keyboard interruption"""
+    # Print the statistics before exiting
+    print_stats()
+    sys.exit(0)
 
-            line = line.split()
+# Register the signal handler for SIGINT (CTRL + C)
 
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
 
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
+signal.signal(signal.SIGINT, signal_handler)
 
-        print_stats(size, status_codes)
+# Loop through each line in stdin
+for line in sys.stdin:
+    # Split the line by spaces
+    tokens = line.split()
+    # If the tokens are not empty
+    if tokens:
+        # Increment the line count
+        line_count += 1
+        # Try to get the status code and the file size from the last two tokens
+        try:
+            status_code = tokens[-2]
+            file_size = int(tokens[-1])
+            # Add the file size to the total size
+            total_size += file_size
+            # If the status code is in the dictionary, increment its count
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except Exception:
+            # Ignore any exception and continue
+            pass
+    # If the line count is a multiple of 10, print the statistics
+    if line_count % 10 == 0:
+        print_stats()
 
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+# Print the statistics at the end of the loop
+print_stats()
