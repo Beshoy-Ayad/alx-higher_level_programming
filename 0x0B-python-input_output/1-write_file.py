aa6ada61d033478f70db7e25428c537a05442223
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-07-11 14:25:42 -0700

    1.2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,8 +3,10 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file and returns the number of characters written"""
+    """Writes a string to a text file and returns the number of
+    characters written"""
     # Use the with statement to open the file in write mode
     with open(filename, "w", encoding="utf-8") as f:
-        # Write the text to the file and return the number of characters written
+        # Write the text to the file and return the number of
+        # characters written
         return f.write(text)
