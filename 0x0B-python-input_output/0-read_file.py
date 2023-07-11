a2229a7459142b20b62abc472596d8cad09a4665
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-07-11 14:20:24 -0700

    0.3

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+"""module to read a text file"""
+
 
 def read_file(filename=""):
-    """Reads a text file and prints it to stdout"""
-    # Use the with statement to open the file in read mode
-    with open(filename, "r", encoding="utf-8") as f:
-        # Loop through each line in the file
+    """reads a text file and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
         for line in f:
-            # Print the line to stdout, end="" prevents adding a new line
             print(line, end="")
