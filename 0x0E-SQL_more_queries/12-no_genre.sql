2be6153251f96bbeb5cc649d22d2e11db573410c
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-08-16 14:22:45 -0700

    12.1

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
 -- List all shows without a genre linked
-SELECT shows.title, show_genres.genre_id
-FROM shows
-LEFT JOIN show_genres ON shows.id = show_genres.show_id
-WHERE show_genres.genre_id IS NULL
-ORDER BY shows.title ASC, show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
