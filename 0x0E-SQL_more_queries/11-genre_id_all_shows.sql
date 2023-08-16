2f919d866961ad8a7cfa9290f58b1e343eae9d48
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-08-16 14:24:20 -0700

    11.2

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- List all shows with their genre ids
-SELECT shows.title, show_genres.genre_id
-FROM shows
-LEFT JOIN show_genres ON shows.id = show_genres.show_id
-ORDER BY shows.title ASC, show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
