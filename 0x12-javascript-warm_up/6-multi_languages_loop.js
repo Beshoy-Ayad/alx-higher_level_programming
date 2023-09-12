e39eb46ec2cd4f90dc802e4731751fa891473d09
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-09-12 02:30:56 -0700

    fixing

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const sent of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+for (const sent of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(sent);
 }
