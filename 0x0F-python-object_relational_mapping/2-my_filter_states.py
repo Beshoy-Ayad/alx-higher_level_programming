80ffd3d441348ed36f7f5efeb76994cc774aac6f
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-09-16 08:34:48 -0700

    2.8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
 """
-This script displays all values in the states table of hbtn_0e_0_usa where name matches the argument in a case-sensitive way.
-
-Arguments: mysql username, mysql password, database name, state name
+return matching states
+parameters given to script: username, password, database, state to match
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+
+    # connect to database
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
 
-    state_name = argv[4]
-
-    cur = db.cursor()
-
-    # Use %s placeholder and BINARY operator for case-sensitive comparison
-    cur.execute("SELECT * FROM states WHERE BINARY name LIKE %s
-                ORDER BY id ASC", (state_name,))
-
-    # Print all matching rows
-    for row in cur.fetchall():
-        print(row)
-
-    cur.close()
+    # create cursor to exec queries using SQL; match arg given
+    cursor = db.cursor()
+    sql_cmd = """SELECT *
+                 FROM states
+                 WHERE name LIKE '{:s}' ORDER BY id ASC""".format(argv[4])
+    cursor.execute(sql_cmd)
+    for row in cursor.fetchall():
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
     db.close()
