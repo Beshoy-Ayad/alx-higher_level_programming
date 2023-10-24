65fcf204278b909d23234ccec5e0cb406bf2014d
Beshoy.Ayad <Beshoy.Ayad@gmail.com>
2023-10-24 11:38:53 -0700

    0.3

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 const filePath = process.argv.slice(2);
 
-fs.readFile(filePath, 'utf8', (err, data) => {
+fs.readFile(filePath[0], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
