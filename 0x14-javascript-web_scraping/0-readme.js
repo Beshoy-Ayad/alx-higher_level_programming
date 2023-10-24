2563d89c6e1ca0c8845240984014fd3619575686
Beshoy.Ayad <Beshoy.Ayad@gmail.com>
2023-10-24 11:34:53 -0700

    0.1

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+  reads and prints the content of a file
+  first argument is the file path
+ */
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
