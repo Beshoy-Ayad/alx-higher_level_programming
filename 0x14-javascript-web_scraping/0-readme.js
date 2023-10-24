83c4eb2ecb8979b4e3fcc3c8e2e27a9872e87d22
Beshoy.Ayad <Beshoy.Ayad@gmail.com>
2023-10-24 11:40:35 -0700

    0.4

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,14 +3,11 @@
   reads and prints the content of a file
   first argument is the file path
  */
-
 const fs = require('fs');
-
 const filePath = process.argv.slice(2);
-
 fs.readFile(filePath[0], 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     console.log(data);
   }
