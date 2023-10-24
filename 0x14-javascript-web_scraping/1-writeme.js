a10009af4d92a0050a883560f0a43d8376fc9c39
Beshoy.Ayad <Beshoy.Ayad@gmail.com>
2023-10-24 11:47:33 -0700

    1.1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/**
+  reads and prints the content of a file
+  first argument is the file path
+ */
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(`Content written to ${filePath}`);
+});
